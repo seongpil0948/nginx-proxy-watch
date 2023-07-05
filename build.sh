c1753af5eb7a7320f7d0e05ec6967290c738db71
주혁노 <kindhn@naver.com>
2023-07-05 14:19:07 +0900

    build 수정

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t nginx-proxy-watch .
-docker tag nginx-proxy-watch docker.shop.co.kr/theshop-front/nginx-proxy-watch:1.0
+docker build -t docker.shop.co.kr/theshop-front/nginx-proxy-watch:1.0 .
+# docker tag nginx-proxy-watch docker.shop.co.kr/theshop-front/nginx-proxy-watch:1.0
 docker push docker.shop.co.kr/theshop-front/nginx-proxy-watch:1.0
