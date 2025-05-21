6454b4387c4df8b4cda899518b8a34f2c2002a1c
Seongpil Choi <seongpil0948@gmail.com>
2025-05-21 16:55:10 +0900

    fix: error

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=2.0.0
+VERSION=2.0.3
 docker build -t docker.shop.co.kr/theshop-front/nginx-proxy-watch:$VERSION .
 # docker tag nginx-proxy-watch docker.shop.co.kr/theshop-front/nginx-proxy-watch:1.0
 docker push docker.shop.co.kr/theshop-front/nginx-proxy-watch:$VERSION
