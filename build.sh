01aa69bd4f99418cbc2abea59e972027862af187
Seongpil Choi <seongpil0948@gmail.com>
2025-06-11 15:25:20 +0900

    SSL 보안 최소화

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=2.2.2-rc1
+VERSION=2.2.2-rc2
 docker build -t docker.shop.co.kr/theshop-front/nginx-proxy-watch:$VERSION .
 # docker tag nginx-proxy-watch docker.shop.co.kr/theshop-front/nginx-proxy-watch:1.0
 docker push docker.shop.co.kr/theshop-front/nginx-proxy-watch:$VERSION
